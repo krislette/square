f9f964ae27555686482236904ffc926e1e0867f0
Regina Bonifacio <rejinimikasi06@gmail.com>
2025-01-29 23:09:25 +0800

    Finalize landing design

--- frontend/src/pages/Landing/Landing.tsx
+++ frontend/src/pages/Landing/Landing.tsx
@@ -13,67 +13,19 @@ function LandingPage() {
             high school students, computer science and IT students especially freshmen, and career shifters
             who are new to coding.
           </p>
-          
-          <div className="button-container">
-            <Link 
-              to="/home" 
-              style={{ textDecoration: "none", color: "inherit" }} 
-              className="custom__button"
-            >
-            <div className="custom__button">              
-            <span className="button__background"></span>
-              <div className="button__content">
-                <span>Go to Terminal</span>
-                <svg
-                  fill="none"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  width="16"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10.75 8.75L14.25 12L10.75 15.25"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                  />
-                </svg>
-              </div>
-            </div>
-            </Link>
 
-            <a 
-                href="https://drive.google.com/paper-link" 
-                target="_blank" 
-                rel="noopener noreferrer" 
-                className="custom__button">
-                <span className="button__background"></span>
-                    <div className="button__content">
-                        <span>View Our Paper</span>
-                            <svg
-                                fill="none"
-                                height="16"
-                                 viewBox="0 0 24 24"
-                                width="16"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                   d="M10.75 8.75L14.25 12L10.75 15.25"
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="1.5"
-                                />
-                            </svg>
-                        </div>
-               </a>
+          <div className="button-group">
+            <Link to="/home" className="btn primary">
+              Go to Terminal
+            </Link>
+            <a href="/paper.pdf" target="_blank" rel="noopener noreferrer" className="btn secondary">
+              View Our Paper
+            </a>
           </div>
         </div>
 
         <div className="square__header-image">
-          {/* ! Put image here */}
-          <img src="sqr-logo.png" alt="Square Programming" />
+          <img src="sqr.png" alt="Square Programming" />
         </div>
       </div>
     </div>
