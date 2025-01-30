c3ffe374236279e91af820a7cb2f6feb4e66d46d
Regina Bonifacio <rejinimikasi06@gmail.com>
2025-01-30 10:59:46 +0800

    Updated home page and navbar styles

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,23 +1,23 @@
-import { NavLink, useLocation } from "react-router-dom"
-import { FaRegSquare } from "react-icons/fa";
-import "./NavBar.css"
+import { NavLink, useLocation } from "react-router-dom";
+import "./NavBar.css";
 
-export default function NavBar(){
-    const location = useLocation();
+export default function NavBar() {
+  const location = useLocation();
 
-    return(
-        <header id="navbar">
-            <div className="navbar-title">
-                <FaRegSquare size={50} />
-                <h1>Square</h1>
-            </div>
-            <nav className="navbar-tabs">
-                <NavLink 
-                    to="/home" 
-                    className={location.pathname == "/" ? "active" : ""}>
-                    Terminal
-                </NavLink>
-            </nav>
-        </header>
-    )
+  return (
+    <header id="navbar">
+      <div className="navbar-title">
+        <img src="sqr.png" alt="Square Logo" width={50} height={50} />
+        <h1>Square</h1>
+      </div>
+      <nav className="navbar-tabs">
+        <NavLink
+          to="/home"
+          className={location.pathname === "/" ? "active" : ""}
+        >
+          Terminal
+        </NavLink>
+      </nav>
+    </header>
+  );
 }
