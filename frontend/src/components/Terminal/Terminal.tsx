01b67b5dc0e404d5723ce614ff101f5224655224
Regina Bonifacio <rejinimikasi06@gmail.com>
2025-02-02 21:19:03 +0800

    Fixed environment variable access in fetch request with the help of Acelle

--- frontend/src/components/Terminal/Terminal.tsx
+++ frontend/src/components/Terminal/Terminal.tsx
@@ -43,7 +43,7 @@ my_var: string = "hello world!"`);
     try {
       setIsLoading(true);
 
-      const response = await fetch("http://127.0.0.1:8000/tokenize", {
+      const response = await fetch(import.meta.env.VITE_API_URL, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
